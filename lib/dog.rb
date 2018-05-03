98210e580332123866e8e222eece5e2d95044a0e
Leticia Molina <letimolina@gmail.com>
2018-05-03 00:23:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,13 @@ class Dog
   def initialize(name)
     @name = name
   end
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
   def initialize(breed)
     @breed = breed
   end
